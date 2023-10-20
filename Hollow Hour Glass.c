d5bb6d015ba7415164a042d5ef4753f09f1c096c
KAMALA KANNAN N G <111722165+kamalakannanngk@users.noreply.github.com>
2023-10-20 23:35:23 +0530

    Add files via upload

---
+++ Hollow Hour Glass.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+	int i,j,n;
+	scanf("%d",&n);
+	for(i=n; i>=1; i--){
+		for(j=1; j<=n-i; j++){
+			printf(" ");
+		}
+		for(j=1; j<=i; j++){
+			if(j==1 || j==i || i==n){
+				printf("* ");
+			}
+			else{
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+	for(i=1+1; i<=n; i++){
+		for(j=1; j<=n-i; j++){
+			printf(" ");
+		}
+		for(j=1; j<=i; j++){
+			if(j==1 || j==i || i==n){
+				printf("* ");
+			}
+			else{
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+}
